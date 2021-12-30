52af691c879ecea06f4c08a291085a684cacb6c0
fukuzo <fukuzo9431@gmail.com>
2021-12-30 19:02:41 +0900

    upd

--- security.php
+++ security.php
@@ -2,3 +2,21 @@
 //
 //
 //
+
+
+//参考資料)
+// https://qiita.com/mpyw/items/2f9955db1c02eeef43ea
+//
+//
+//
+//
+
+function h($str)
+{
+    //XSS対策
+
+    // ex)
+    // <p><?php echo h($str); ?></p>
+    //
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
