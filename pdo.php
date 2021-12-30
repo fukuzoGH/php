45279d7fc79d691516dd8c8e3a5eb9e8516c8923
fukuzo <fukuzo9431@gmail.com>
2021-12-30 18:17:11 +0900

    upd

--- pdo.php
+++ pdo.php
@@ -1,2 +1,21 @@
 <?php
 
+function test01() {
+    try {
+        $addr = 'mysql:host=localhost; dbname=testdb; port=1306; ';
+        $user = 'root';
+        $pass = 'password';
+
+        // 接続
+        $pdo = new PDO($addr, $user, $pass);
+        
+        $state = $pdo->query('SET NAMES utf-8');
+    }
+    catch(PDOException $e){
+            echo "database open error: $addr\n $user\n" . $e->getMessage();
+    }
+        $pdo = NULL;    // 切断
+}
+
+//データベースに接続する
+test01();
